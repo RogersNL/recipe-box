f6205e09246fc8822ee6c15a65c422636a251bba
Colin Bates, Nick Rogers and Swati Sahay <swati@>
2018-08-14 14:19:28 -0700

    Create recipe box.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Recipe Box';
+  recipe: Recipe[] = [
+    new Recipe('Pizza', ['Cheese', 'Tomato Sauce', 'Pepperoni', 'Pizza Dough'],['Put it together', 'Bake', 'Eat']),
+    new Recipe('Chili bean', ['Bean'], ['Microwave', 'Eat']),
+    new Recipe('Hummus', ['Chick Pea', 'spice things'], ['Dip chips', 'Eat'])
+  ];
 }
